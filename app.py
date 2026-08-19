b4b9b1986271e53888edf187b67939686975508f
hjkhj <sadewa@gmail.com>
2026-08-19 14:04:53 -0500

    Se cambio el index, por practica de respaso, adicionalmente se agrego una carpeta de estilos y otra ruta

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ name = "Cansona"
 def hello_world():
     return render_template('index.html', person=name)
 
+@app.route("/actividad")
+def actividad():
+    return render_template( 'ejercicioClase.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
